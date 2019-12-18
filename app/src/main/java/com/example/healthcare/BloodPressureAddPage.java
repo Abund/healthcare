b39ca958f7c740955e9a77f3ad180c54eae2bb0c
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-12-18 11:50:43 +0100

    Coded the blood pressure add page to enable user add blood pressure readings
    created the blood pressure add activity to enable people add calibrated values to the app

---
+++ app/src/main/java/com/example/healthcare/BloodPressureAddPage.java
@@ -0,0 +1,14 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BloodPressureAddPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blood_pressure_add_page);
+    }
+}
