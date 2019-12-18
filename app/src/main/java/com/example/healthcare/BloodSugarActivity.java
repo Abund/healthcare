869d0604f98b6ed6f9ed4d39f76fd01c9487bbb2
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-12-18 12:26:16 +0100

    Added the back-end code for the blood sugar page
    Added the back-end code for the blood sugar page including its the functionality for the graph

--- app/src/main/java/com/example/healthcare/BloodSugarActivity.java
+++ app/src/main/java/com/example/healthcare/BloodSugarActivity.java
@@ -1,14 +1,204 @@
 package com.example.healthcare;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class BloodSugarActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class BloodSugarActivity extends Fragment {
+
+    private LineChart lineChart;
+    private RecyclerView mRecycler;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<BloodSugar> data;
+    private ArrayList<String> mData;
+    private DatabaseReference myRef;
+    private BloodSugarAdapter bloodSugarAdapter;
+    private Button suggestionBS;
+    private TextView alternate;
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState) {
+        view= inflater.inflate(R.layout.activity_blood_sugar,container,false);
+        super.onCreate(savedInstanceState);
+
+        lineChart =(LineChart) view.findViewById(R.id.lineChart2);
+        alternate =(TextView) view.findViewById(R.id.alternate);
+        suggestionBS =(Button) view.findViewById(R.id.suggestionBS);
+        mRecycler = (RecyclerView) view.findViewById(R.id.recyclerViewBSA);
+        //lineChart.setOnChartGestureListener(BloodpressureActivity.this);
+        //lineChart.setOnChartValueSelectedListener(BloodpressureActivity.this);
+
+
+        data = new ArrayList<BloodSugar>();
+        mRecycler.setHasFixedSize(true);
+        mRecycler.setLayoutManager(new LinearLayoutManager(getActivity().getBaseContext()));
+        myRef = FirebaseDatabase.getInstance().getReference().child("BloodSugar").child(FirebaseAuth.getInstance().getUid());
+        myRef.keepSynced(true);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                    if (dataSnapshot.getChildrenCount() == 0){
+                        return;
+                    }
+                    BloodSugar bloodSugar = dataSnapshot1.getValue(BloodSugar.class);
+                    data.add(bloodSugar);
+                }
+                lineChart=sendata(lineChart,data);
+                bloodSugarAdapter = new BloodSugarAdapter(data,getActivity().getBaseContext());
+                mRecycler.setAdapter(bloodSugarAdapter);
+                if(data.isEmpty()){
+                    alternate.setVisibility(View.VISIBLE);
+                    alternate.setText("Please add your data");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity().getBaseContext(),"Oppss... something went wrong",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        FloatingActionButton fab = view.findViewById(R.id.floatingActionButton1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent at = new Intent(getActivity().getBaseContext(), BloodSugarAddPage.class);
+                startActivity(at);
+            }
+        });
+        suggestionBS.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getActivity().getBaseContext(), BloodSugarPopUp.class));
+            }
+        });
+        return view;
+    }
+
+    private LineChart sendata(LineChart lineChart,ArrayList<BloodSugar> data) {
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+
+        LimitLine upperLimit = new LimitLine(130f,"DANGER");
+        upperLimit.setLineWidth(4f);
+        upperLimit.enableDashedLine(10f,10f,0);
+        upperLimit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        upperLimit.setTextSize(15f);
+
+        LimitLine lowerLimit = new LimitLine(112f,"Too Low");
+        upperLimit.setLineWidth(4f);
+        upperLimit.enableDashedLine(10f,10f,0);
+        upperLimit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        upperLimit.setTextSize(15f);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.removeAllLimitLines();
+        leftAxis.addLimitLine(upperLimit);
+        leftAxis.addLimitLine(lowerLimit);
+        //leftAxis.setAxisMaximum(100f);
+        //leftAxis.setAxisMinimum(25f);
+        leftAxis.enableGridDashedLine(10f,10f,0);
+        leftAxis.setDrawLimitLinesBehindData(true);
+        lineChart.getAxisRight().setEnabled(false);
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+
+
+//        final String[] months = new String[]{"Feb", "Feb", "Mar", "Apr", "Mar", "Apr"};
+
+        //final String[] months = new String[data.size()];
+//        for(int i =0;i<data.size();i++){
+//            months[i]=data.get(i).getDate();
+//            //yValues.add(new Entry(i, data.get(i).getDiastolicPressure()));
+//        }
+
+//        ValueFormatter formatter = new ValueFormatter() {
+//            @Override
+//            public String getAxisLabel(float value, AxisBase axis) {
+//                return months[(int) value];
+//            }
+//        };
+//        XAxis xAxis = lineChart.getXAxis();
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xAxis.setGranularity(1f);
+//        xAxis.setValueFormatter(formatter);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        float f= (float)data.get(0).getConcentrationSugar();
+        Log.e("eeeeeeeeeeeeeeeeee",""+f);
+
+        for(int i =0;i<data.size();i++){
+            float f1 = (float)data.get(i).getConcentrationSugar();
+            yValues.add(new Entry(i, f1));
+            Log.e("eeeeeeeeeeeeeeeeee",""+data.get(i).getConcentrationSugar());
+        }
+
+        LineDataSet set1 = new LineDataSet(yValues,"Data set 1");
+        set1.setFillAlpha(110);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data1 = new LineData(dataSets);
+        lineChart.setData(data1);
+        return lineChart;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blood_sugar);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("HealthCare");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public void onRefresh() {
+        Toast.makeText(getActivity(), "Fragment : Refresh called.",
+                Toast.LENGTH_SHORT).show();
     }
 }
