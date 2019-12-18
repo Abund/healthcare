e65450772848cbde554feafb1cea1901aabffa4d
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-12-18 14:08:20 +0100

    Added the goal add page/activity to enable users alter tha application goals
    I added the goal activity page to enable user to add adjustment to the application goals

---
+++ app/src/main/java/com/example/healthcare/GoalAddPage.java
@@ -0,0 +1,14 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalAddPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_add_page);
+    }
+}
