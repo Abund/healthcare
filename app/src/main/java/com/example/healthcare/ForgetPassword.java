13f10c200e58116cdefad90e81e5db46204ee9f9
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-12-27 19:05:30 +0100

    Coded the back end logic for the password forget page.

--- app/src/main/java/com/example/healthcare/ForgetPassword.java
+++ app/src/main/java/com/example/healthcare/ForgetPassword.java
@@ -1,14 +1,49 @@
 package com.example.healthcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgetPassword extends AppCompatActivity {
+
+    private EditText email;
+    private Button send;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        send  =(Button) findViewById(R.id.fsend);
+        email  =(EditText) findViewById(R.id.femail);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        send.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                firebaseAuth.sendPasswordResetEmail(email.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(ForgetPassword.this,"Password reset link sent to your email",Toast.LENGTH_LONG).show();
+                                }else {
+                                    Toast.makeText(ForgetPassword.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
